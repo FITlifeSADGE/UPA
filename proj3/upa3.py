a8c8044fe491341c64f8a4a68964b783bd0920fe
Lukáš Kaprál <80596523+FITlifeSADGE@users.noreply.github.com>
2023-11-25 20:41:26 +0100

    Added parallel requests

--- proj3/upa3.py
+++ proj3/upa3.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
 
 def scrape_product_urls(url, max_items=100):
     all_product_urls = []
@@ -47,61 +49,112 @@ def scrape_product_urls(url, max_items=100):
 
     return all_product_urls[:max_items]
 
-def extract_data(url):
+async def extract_data(url):
     try:
         # Fetch the HTML content of the URL
-        response = requests.get(url)
-        response.raise_for_status()
-
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
+        async with aiohttp.ClientSession() as s:
+            async with s.get(url, timeout=10) as r:
+                html = await r.text()
+                if r.ok: #if return code is under 400 go on
+                    if html != "":
+                        soup = BeautifulSoup(html, 'html.parser')
         
-        price = soup.find('p', class_="sitePrice")
-        book_name = soup.find('h1', itemprop="name")
-        
-        if book_name:
-                for span in book_name.find_all('span'):
-                    span.decompose()
-                    
-        product_info_div = soup.find('div', class_='productInfo')
-        
-        format_info = product_info_div.find('li', class_='format')       
-        publisher_info = product_info_div.find('li', class_='publisher')
-        ISBN_info = product_info_div.find('li', class_='EAN')
-        
-        publisher_info = publisher_info.text.strip() if publisher_info else None
-        ISBN_info = ISBN_info.text.strip() if ISBN_info else None
-        format_info = format_info.text.strip() if format_info else None
-        book_name = book_name.text.strip() if book_name else None
-        price = price.text.strip() if price else None
-        
-        return price, book_name, publisher_info, ISBN_info, format_info
-            
-
+                        price = soup.find('p', class_="sitePrice")
+                        book_name = soup.find('h1', itemprop="name")
+                        
+                        if book_name:
+                                for span in book_name.find_all('span'):
+                                    span.decompose()
+                                    
+                        product_info_div = soup.find('div', class_='productInfo')
+                        
+                        format_info = product_info_div.find('li', class_='format')       
+                        publisher_info = product_info_div.find('li', class_='publisher')
+                        ISBN_info = product_info_div.find('li', class_='EAN')
+                        
+                        publisher_info = publisher_info.text.strip() if publisher_info else None
+                        ISBN_info = ISBN_info.text.strip() if ISBN_info else None
+                        format_info = format_info.text.strip() if format_info else None
+                        book_name = book_name.text.strip() if book_name else None
+                        price = price.text.strip() if price else None
+                        
+                        print(f"URL: {url}")
+                        print(f"Price paid: {price}")
+                        print(f"Book name: {book_name}")
+                        print(f"{publisher_info}")
+                        print(f"{ISBN_info}")
+                        print(f"{format_info}")
+                        print("----")     
     except Exception as e:
         print(f"Error processing URL {url}: {e}")
         return None, None, None, None, None
+
+# 30 requests at a time
+# async def extract_data(url, semaphore):
+#     try:
+#         async with semaphore:
+#             # Fetch the HTML content of the URL
+#             async with aiohttp.ClientSession() as s:
+#                 async with s.get(url, timeout=10) as r:
+#                     html = await r.text()
+#                     if r.ok: #if return code is under 400 go on
+#                         if html != "":
+#                             soup = BeautifulSoup(html, 'html.parser')
+            
+#                             price = soup.find('p', class_="sitePrice")
+#                             book_name = soup.find('h1', itemprop="name")
+                            
+#                             if book_name:
+#                                     for span in book_name.find_all('span'):
+#                                         span.decompose()
+                                        
+#                             product_info_div = soup.find('div', class_='productInfo')
+                            
+#                             format_info = product_info_div.find('li', class_='format')       
+#                             publisher_info = product_info_div.find('li', class_='publisher')
+#                             ISBN_info = product_info_div.find('li', class_='EAN')
+                            
+#                             publisher_info = publisher_info.text.strip() if publisher_info else None
+#                             ISBN_info = ISBN_info.text.strip() if ISBN_info else None
+#                             format_info = format_info.text.strip() if format_info else None
+#                             book_name = book_name.text.strip() if book_name else None
+#                             price = price.text.strip() if price else None
+                            
+#                             print(f"URL: {url}")
+#                             print(f"Price paid: {price}")
+#                             print(f"Book name: {book_name}")
+#                             print(f"{publisher_info}")
+#                             print(f"{ISBN_info}")
+#                             print(f"{format_info}")
+#                             print("----")     
+#     except Exception as e:
+#         print(f"Error processing URL {url}: {e}")
+#         return None, None, None, None, None
 
 
 # Specify the URL of the first page
 url = 'https://www.hive.co.uk/Search/eBooks/Fiction?fq=01124-121934'
 
-# Scrape and select 100 random product URLs
 selected_product_urls = scrape_product_urls(url, max_items=100)
-# for i in selected_product_urls:
-#     print(i)
 
-for url in selected_product_urls:
-    price, book_name, publisher, ISBN, format = extract_data(url)
-    
-    publisher = publisher.split(':')[-1].strip() if publisher else None
-    ISBN = ISBN.split(':')[-1].strip() if ISBN else None
-    format = format.split(':')[-1].strip() if format else None
-    print(f"URL: {url}")
-    print(f"Price paid: {price}")
-    print(f"Book name : {book_name}")
-    print(f"Publisher : {publisher}")
-    print(f"ISBN : {ISBN}")
-    print(f"Format : {format}")
-    print("----")
+
+# 30 at a time
+# async def run(selected_product_urls):
+#         semaphore = asyncio.Semaphore(30)
+#         tasks = list()
+#         for domain in selected_product_urls:
+#             #await urlQ.put(i)
+#             task = asyncio.create_task(extract_data(domain, semaphore))
+#             tasks.append(task) 
+#         await asyncio.gather(*tasks)
+        
+async def run(selected_product_urls):
+        tasks = list()
+        for domain in selected_product_urls:
+            task = asyncio.create_task(extract_data(domain))
+            tasks.append(task) 
+        await asyncio.gather(*tasks)
+
+asyncio.run(run(selected_product_urls))
+
 
